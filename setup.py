9e6ea80f990879aff03f27cea12a8b87876e6f45
Zewen Kelvin Tuong <z.tuong@uq.edu.au>
2024-10-03 11:57:56 +1000

    oops

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme_file:
 
 setup(
     name="ktbunya",
-    version="0.6.0",
+    version="0.7.0",
     author="zktuong",
     author_email="z.tuong@uq.edu.au",
     description="Simple scripts to help create an interactive session to run jupyter notebooks using UQ's Bunya/Wiener compute.",
@@ -14,12 +14,8 @@ setup(
     url="https://github.com/zktuong/ktbunya/",
     packages=find_packages(),
     setup_requires=["setuptools>=45", "wheel", "setuptools_scm[toml]>=6.0"],
-    package_data={
-        "ktbunya": ["bin/jpynb", "bin/jpynbw", "bin/ixcpu", "bin/ixgpu", "bin/killssh"]
-    },
-    data_files=[
-        ("bin", ["bin/jpynb", "bin/jpynbw", "bin/ixcpu", "bin/ixgpu", "bin/killssh"])
-    ],
+    package_data={"ktbunya": ["bin/jpynb", "bin/ixcpu", "bin/ixgpu"]},
+    data_files=[("bin", ["bin/jpynb", "bin/ixcpu", "bin/ixgpu"])],
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3.10",
