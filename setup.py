5a8e28f14fe882b683c73106edecc62871356c74
Zewen Kelvin Tuong <z.tuong@uq.edu.au>
2024-11-11 17:18:58 +1000

    map cuda path and add a dry run option (#13)
    * add a dry run option
    
    * add
    
    * Update ixgpu
    
    * Update README.md
    
    * Create cudapath
    
    * Update cudapath
    
    * now to make it automatically run
    
    * place back the space
    
    * rename dry run
    
    * Update set_cuda_mod
    
    * Update setup.py
    
    * job dies straight away
    
    * can't run straightaway
    
    * get rid of set_cuda_mod first
    
    * now add it back
    
    * try and add it back
    
    * try manually
    
    * Update set_cuda_mod
    
    * Update set_cuda_mod
    
    * Update set_cuda_mod
    
    * Update set_cuda_mod
    
    * Update set_cuda_mod
    
    * Update set_cuda_mod
    
    * try this
    
    * rename it
    
    * Update README.md
    
    * Update setup.py
    
    * change to cudamap instead

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ with open("README.md", "r") as readme_file:
 
 setup(
     name="ktbunya",
-    version="0.8.0",
+    version="0.9.0",
     author="zktuong",
     author_email="z.tuong@uq.edu.au",
-    description="Simple scripts to help create an interactive session to run jupyter notebooks using UQ's Bunya/Wiener compute.",
+    description="Simple scripts to help create an interactive session to run jupyter notebooks using UQ's Bunya compute.",
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/zktuong/ktbunya/",
     packages=find_packages(),
     setup_requires=["setuptools>=45", "wheel", "setuptools_scm[toml]>=6.0"],
-    package_data={"ktbunya": ["bin/jpynb", "bin/ixcpu", "bin/ixgpu"]},
-    data_files=[("bin", ["bin/jpynb", "bin/ixcpu", "bin/ixgpu"])],
+    package_data={"ktbunya": ["bin/jpynb", "bin/ixcpu", "bin/ixgpu", "bin/cudamap"]},
+    data_files=[("bin", ["bin/jpynb", "bin/ixcpu", "bin/ixgpu", "bin/cudamap"])],
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3.10",
